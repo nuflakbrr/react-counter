3bd19c2437b56c8a90d304aea97855a7281597c9
Naufal Akbar <kbrnugroho@gmail.com>
2021-08-15 17:30:10 +0700

    change background color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="app">
+    <div className="App">
       <h1 className={count > 0 ? "positive" : count < 0 ? "negative" : null}>{count}</h1>
       <div className="button__wrapper">
         <button onClick={() => setCount(count - 1)}>-</button>
